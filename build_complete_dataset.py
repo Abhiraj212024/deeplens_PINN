0bb9ad48e9c165d373e2b9a61572cae60b23942f
Abhiraj212024 <abhirajraje1@gmail.com>
2026-03-17 02:16:00 +0530

    added TESTING_DATA_PATH and TRAIN_DATA_PATH variables for easier reproduction

--- build_complete_dataset.py
+++ build_complete_dataset.py
@@ -2,11 +2,11 @@ import numpy as np
 import glob
 import os
 
+TRAINING_DATA_PATH = "dataset/train" # Add training data path here
+TEST_DATA_PATH = "dataset/val" # Add test data path here
+
 def load_class(class_dir, label):
     raw_files = sorted(glob.glob(os.path.join(class_dir, "*.npy")))
-    # pre_files = sorted(glob.glob(os.path.join(class_dir, "*.npy")))
-
-    # assert len(raw_files) == len(pre_files)
 
     X = []
     y = []
@@ -22,9 +22,9 @@ def load_class(class_dir, label):
     return np.asarray(X, dtype=np.float32), np.asarray(y, dtype=np.int64)
 
 
-X_no_train, y_no_train = load_class("dataset/train/no", label=0)
-X_cdm_train, y_cdm_train = load_class("dataset/train/sphere", label=1)
-X_ax_train , y_ax_train  = load_class("dataset/train/vort", label=2)
+X_no_train, y_no_train = load_class(f"{TRAINING_DATA_PATH}/no", label=0)
+X_cdm_train, y_cdm_train = load_class(f"{TRAINING_DATA_PATH}/sphere", label=1)
+X_ax_train , y_ax_train  = load_class(f"{TRAINING_DATA_PATH}/vort", label=2)
 
 X_train = np.concatenate([X_no_train, X_cdm_train, X_ax_train], axis=0)
 y_train = np.concatenate([y_no_train, y_cdm_train, y_ax_train], axis=0)
@@ -33,9 +33,9 @@ print(X_train.shape, y_train.shape)
 np.save("X_train.npy", X_train)
 np.save("y_train.npy", y_train)
 
-X_no_test, y_no_test = load_class("dataset/val/no", label=0)
-X_cdm_test, y_cdm_test = load_class("dataset/val/sphere", label=1)
-X_ax_test , y_ax_test  = load_class("dataset/val/vort", label=2)
+X_no_test, y_no_test = load_class(f"{TEST_DATA_PATH}/no", label=0)
+X_cdm_test, y_cdm_test = load_class(f"{TEST_DATA_PATH}/sphere", label=1)
+X_ax_test , y_ax_test  = load_class(f"{TEST_DATA_PATH}/vort", label=2)
 
 X_test = np.concatenate([X_no_test, X_cdm_test, X_ax_test], axis=0)
 y_test = np.concatenate([y_no_test, y_cdm_test, y_ax_test], axis=0)
